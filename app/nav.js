3ec34557c826168ad3504d04abfda938c8de7433
Josh <webdevwannab@gmail.com>
2015-12-17 13:09:53 -0500

    First commit

---
+++ app/nav.js
@@ -0,0 +1,27 @@
+
+
+document.write ('\
+\
+<nav>\
+  <ul>\
+    <li>HTML</li>\
+    <li>CSS</li>\
+      <ul>\
+        <li><a href="#">Selectors</a></li>\
+        <li><a href="#">Specificity</a></li>\
+        <li><a href="#">Layout</a></li>\
+        <li><a href="#">Responsive Design</a></li>\
+        <li><a href="#">Sass</a></li>\
+        <li><a href="#">Background and Borders </a></li>\
+        <li><a href="#">Typography</a></li>\
+        <li><a href="#">UI</a></li>\
+        <li><a href="#">Animations, Transforms, Transitions</a></li>\
+        <li><a href="#">CSS Best Practices</a></li>\
+        <li><a href="#">SMACSS</a></li>\
+      </ul>\
+    <li>Javascript</li>\
+    <li>Jquery</li>\
+  </ul>\
+</nav>\
+\
+');
